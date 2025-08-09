02e8997ff8c98827aff28a406a829c68002dc438
SrinuTirumanisetti <srinutirumanisetti@gmail.com>
2025-08-09 12:06:25 +0000

    Implemented Todo Model

--- src/main/java/com/example/todo/controller/TodoController.java
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -7,3 +7,20 @@
  */
 
 // Write your code here
+package com.example.todo.controller;
+
+import com.example.todo.model.Todo;
+import java.util.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RestController
+public class TodoController{
+    @Autowired
+    public TodoH2Service service;
+
+    @GetMapping("/todos")
+    public List<Todo> getTodoList(){
+        return service.getTodoList();
+    }
+}
